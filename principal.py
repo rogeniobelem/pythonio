a8a4975006cc7d6f36b64e53bdc74ed0fe70f21d
Rogênio Lima Belém <rogeniobelem@gmail.com>
2020-07-05 16:09:35 -0300

    Iterando diretamente a variável 'arquivo_contatos'

--- principal.py
+++ principal.py
@@ -3,7 +3,7 @@
 
 arquivo_contatos = open('dados/contatos.csv', encoding='latin_1')
 
-conteudo = arquivo_contatos.readlines()
+# conteudo = arquivo_contatos.readlines()
 
-for linha in conteudo:
+for linha in arquivo_contatos:
     print(linha, end='')
