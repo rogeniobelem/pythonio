e85116c5f6b385f5590b30b4a66d1601dbb4d968
Rogênio Lima Belém <rogeniobelem@gmail.com>
2020-07-05 17:19:03 -0300

    O método flush

--- testa_escrita.py
+++ testa_escrita.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-arquivo_contatos = open('dados/contatos_escrita.csv', encoding='latin_1', mode='a+')
+arquivo_contatos = open('dados/contatos_escrita.csv', encoding='latin_1', mode='w+')
 
-contato = '11,Carol,carol@carol.com.br\n'
+contatos = ['11,Carol,carol@carol.com.br\n',
+            '12,Ana,ana@ana.com.br\n',
+            '13,Tais,tais@tais.com.br\n',
+            '14,Felipe,felipe@felipe.com.br\n']
 
-arquivo_contatos.write(contato)
+for contato in contatos:
+    arquivo_contatos.write(contato)
+
+arquivo_contatos.flush()
+
+input('Pressione <Enter> para encerrar o programa')
