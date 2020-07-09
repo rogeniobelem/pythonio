fc1844466e12217cd4437b29df6902a48f7e54e6
Rogênio Lima Belém <rogeniobelem@gmail.com>
2020-07-09 18:54:07 -0300

    Utilizando o with

--- testa_concorrencia.py
+++ testa_concorrencia.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-arquivo1 = open('dados/contatos_escrita.csv', encoding='latin_1', mode='w')
-arquivo2 = open('dados/contatos_escrita.csv', encoding='latin_1', mode='w')
-
-
 contato_carol = '11,Carol,carol@carol.com.br\n'
 contato_andreza = '12,Andreza,andreza@andreza.com.br\n'
 
-arquivo1.write(contato_carol)
-arquivo2.write(contato_andreza)
+with open('dados/contatos_escrita.csv', encoding='latin_1', mode='w') as arquivo1:
+    arquivo1.write(contato_carol)
+
+with open('dados/contatos_escrita.csv', encoding='latin_1', mode='a') as arquivo2:
+    arquivo2.write(contato_andreza)
+
+arquivo2.write('Nova linha')
