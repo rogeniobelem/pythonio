168bc932752d7b2ea5a9c4ce1dfe4acc45225643
Rogênio Lima Belém <rogeniobelem@gmail.com>
2020-07-10 10:19:11 -0300

    Convertendo dados para json

--- contatos_utils.py
+++ contatos_utils.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import csv, pickle, json
+import csv
+import pickle
+import json
 from contato import Contato
+
 
 def csv_para_contatos(caminho, encoding='latin_1'):
     contatos = []
@@ -18,12 +21,36 @@ def csv_para_contatos(caminho, encoding='latin_1'):
 
     return contatos
 
+
 def contatos_para_pickle(contatos, caminho):
     with open(caminho, mode='wb') as arquivo:
         pickle.dump(contatos, arquivo)
 
+
 def pickle_para_contatos(caminho):
     with open(caminho, mode='rb') as arquivo:
         contatos = pickle.load(arquivo)
+
+    return contatos
+
+
+def contatos_para_json(contatos, caminho):
+    with open(caminho, mode='w') as arquivo:
+        json.dump(contatos, arquivo, default=_contato_para_json)
+
+
+def _contato_para_json(contato):
+    return contato.__dict__
+
+
+def json_para_contatos(caminho):
+    contatos = []
+
+    with open(caminho) as arquivo:
+        contatos_json = json.load(arquivo)
+
+        for contato in contatos_json:
+            c = Contato(**contato)
+            contatos.append(c)
 
     return contatos
